57ba5918cee7bcb5b222a4b78f511357ae263773
Kwonelee <995546054@qq.com>
2025-09-29 19:57:57 +0800

    Update 00-prepare_base.sh

--- diy/00-prepare_base.sh
+++ diy/00-prepare_base.sh
@@ -5,7 +5,7 @@
 sed -i 's/Os/O2/g' include/target.mk
 
 # 内核版本设置
-cp -rf OpenBox/kernel-6.6/kernel/0001-linux-module-video.patch package/0001-linux-module-video.patch
+cp -rf ../OpenBox/kernel-6.6/kernel/0001-linux-module-video.patch ./package/0001-linux-module-video.patch
 git apply package/0001-linux-module-video.patch
 rm -rf package/0001-linux-module-video.patch
 
@@ -19,24 +19,24 @@ sed -i '/CONFIG_BUILDBOT/d' include/feeds.mk
 sed -i 's/;)\s*\\/; \\/' include/feeds.mk
 
 ### FW4 ###
-cp -rf OpenBox/firewall4/Makefile ./package/network/config/firewall4/Makefile
+cp -rf ../OpenBox/firewall4/Makefile ./package/network/config/firewall4/Makefile
 sed -i 's|$(PROJECT_GIT)/project|https://github.com/openwrt|g' package/network/config/firewall4/Makefile
 mkdir -p package/network/config/firewall4/patches
-patch -p1 < OpenBox/firewall4/100-openwrt-firewall4-add-custom-nft-command-support.patch
-cp -rf OpenBox/firewall4/firewall4_patches package/network/config/firewall4/patches/
+patch -p1 < ../OpenBox/firewall4/100-openwrt-firewall4-add-custom-nft-command-support.patch
+cp -rf ../OpenBox/firewall4/firewall4_patches package/network/config/firewall4/patches/
 
 # libnftnl
 mkdir -p package/libs/libnftnl/patches
-cp -f OpenBox/firewall4/libnftnl/*.patch ./package/libs/libnftnl/patches/
+cp -f ../OpenBox/firewall4/libnftnl/*.patch ./package/libs/libnftnl/patches/
 
 # nftables
 mkdir -p package/network/utils/nftables/patches
-cp -f OpenBox/firewall4/nftables/*.patch ./package/network/utils/nftables/patches/
+cp -f ../OpenBox/firewall4/nftables/*.patch ./package/network/utils/nftables/patches/
 
 # kernel patch
-cp -f OpenBox/kernel-6.6/btf/*.patch ./target/linux/generic/hack-6.6/
-cp -f OpenBox/kernel-6.6/arm/*.patch ./target/linux/generic/hack-6.6/
-cp -f OpenBox/kernel-6.6/net/*.patch ./target/linux/generic/hack-6.6/
+cp -f ../OpenBox/kernel-6.6/btf/*.patch ./target/linux/generic/hack-6.6/
+cp -f ../OpenBox/kernel-6.6/arm/*.patch ./target/linux/generic/hack-6.6/
+cp -f ../OpenBox/kernel-6.6/net/*.patch ./target/linux/generic/hack-6.6/
 
 # FullCone module
 git clone https://git.kejizero.online/zhao/nft-fullcone package/new/nft-fullcone
@@ -51,10 +51,10 @@ git clone https://github.com/sbwml/package_new_natflow package/new/natflow
 git clone https://git.cooluc.com/sbwml/shortcut-fe package/new/shortcut-fe
 
 # BBRv3
-cp -rf OpenBox/kernel-6.6/bbr3/* ./target/linux/generic/backport-6.6/
+cp -rf ../OpenBox/kernel-6.6/bbr3/* ./target/linux/generic/backport-6.6/
 
 # LRNG
-cp -rf OpenBox/kernel-6.6/lrng/* ./target/linux/generic/hack-6.6/
+cp -rf ../OpenBox/kernel-6.6/lrng/* ./target/linux/generic/hack-6.6/
 echo '
 # CONFIG_RANDOM_DEFAULT_IMPL is not set
 CONFIG_LRNG=y
@@ -77,44 +77,44 @@ wget -qO - https://github.com/immortalwrt/immortalwrt/commit/9d852a0.patch | pat
 
 ### Other Kernel Hack 部分 ###
 # make olddefconfig
-patch -p1 < OpenBox/kernel-6.6/kernel/0003-include-kernel-defaults.mk.patch
+patch -p1 < ../OpenBox/kernel-6.6/kernel/0003-include-kernel-defaults.mk.patch
 # igc-fix
-cp -rf OpenBox/kernel-6.6/igc-fix/* ./target/linux/x86/patches-6.6/
+cp -rf ../OpenBox/kernel-6.6/igc-fix/* ./target/linux/x86/patches-6.6/
 # btf
-cp -rf OpenBox/kernel-6.6/btf/* ./target/linux/generic/hack-6.6/
+cp -rf ../OpenBox/kernel-6.6/btf/* ./target/linux/generic/hack-6.6/
 
 ### 个性化修改 ###
 sed -i "s/192.168.1.1/10.0.0.1/g" package/base-files/files/bin/config_generate
 
 sed -i 's/OpenWrt/ZeroWrt/' package/base-files/files/bin/config_generate
 
-cp -rf OpenBox/doc/base-files/etc/banner ./package/base-files/files/etc/banner
+cp -rf ../OpenBox/doc/base-files/etc/banner ./package/base-files/files/etc/banner
 
 # luci-mod extra
 pushd feeds/luci
-patch -p1 <OpenBox/firewall4/luci-24.10/0001-luci-app-firewall-add-nft-fullcone-and-bcm-fullcone-.patch
-patch -p1 <OpenBox/firewall4/luci-24.10/0002-luci-app-firewall-add-shortcut-fe-option.patch
-patch -p1 <OpenBox/firewall4/luci-24.10/0003-luci-app-firewall-add-ipv6-nat-option.patch
-patch -p1 <OpenBox/firewall4/luci-24.10/0004-luci-add-firewall-add-custom-nft-rule-support.patch
-patch -p1 <OpenBox/firewall4/luci-24.10/0005-luci-app-firewall-add-natflow-offload-support.patch
-patch -p1 <OpenBox/firewall4/luci-24.10/0006-luci-app-firewall-enable-hardware-offload-only-on-de.patch
-patch -p1 <OpenBox/firewall4/luci-24.10/0007-luci-app-firewall-add-fullcone6-option-for-nftables-.patch
+patch -p1 <../../../OpenBox/firewall4/luci-24.10/0001-luci-app-firewall-add-nft-fullcone-and-bcm-fullcone-.patch
+patch -p1 <../../../OpenBox/firewall4/luci-24.10/0002-luci-app-firewall-add-shortcut-fe-option.patch
+patch -p1 <../../../OpenBox/firewall4/luci-24.10/0003-luci-app-firewall-add-ipv6-nat-option.patch
+patch -p1 <../../../OpenBox/firewall4/luci-24.10/0004-luci-add-firewall-add-custom-nft-rule-support.patch
+patch -p1 <../../../OpenBox/firewall4/luci-24.10/0005-luci-app-firewall-add-natflow-offload-support.patch
+patch -p1 <../../../OpenBox/firewall4/luci-24.10/0006-luci-app-firewall-enable-hardware-offload-only-on-de.patch
+patch -p1 <../../../OpenBox/firewall4/luci-24.10/0007-luci-app-firewall-add-fullcone6-option-for-nftables-.patch
 popd
 
 # luci-mod extra
 pushd feeds/luci
-patch -p1 <OpenBox/luci/0001-luci-mod-system-add-modal-overlay-dialog-to-reboot.patch
-patch -p1 <OpenBox/luci/0002-luci-mod-status-displays-actual-process-memory-usage.patch
-patch -p1 <OpenBox/luci/0003-luci-mod-status-storage-index-applicable-only-to-val.patch
-patch -p1 <OpenBox/luci/0004-luci-mod-status-firewall-disable-legacy-firewall-rul.patch
-patch -p1 <OpenBox/luci/0005-luci-mod-system-add-refresh-interval-setting.patch
-patch -p1 <OpenBox/luci/0006-luci-mod-system-mounts-add-docker-directory-mount-po.patch
-patch -p1 <OpenBox/luci/0007-luci-mod-system-add-ucitrack-luci-mod-system-zram.js.patch
+patch -p1 <../../../OpenBox/luci/0001-luci-mod-system-add-modal-overlay-dialog-to-reboot.patch
+patch -p1 <../../../OpenBox/luci/0002-luci-mod-status-displays-actual-process-memory-usage.patch
+patch -p1 <../../../OpenBox/luci/0003-luci-mod-status-storage-index-applicable-only-to-val.patch
+patch -p1 <../../../OpenBox/luci/0004-luci-mod-status-firewall-disable-legacy-firewall-rul.patch
+patch -p1 <../../../OpenBox/luci/0005-luci-mod-system-add-refresh-interval-setting.patch
+patch -p1 <../../../OpenBox/luci/0006-luci-mod-system-mounts-add-docker-directory-mount-po.patch
+patch -p1 <../../../OpenBox/luci/0007-luci-mod-system-add-ucitrack-luci-mod-system-zram.js.patch
 popd
 
 # opkg
 mkdir -p package/system/opkg/patches
-cp -rf OpenBox/opkg/* ./package/system/opkg/patches/
+cp -rf ../OpenBox/opkg/* ./package/system/opkg/patches/
 
 # TTYD
 sed -i 's/services/system/g' feeds/luci/applications/luci-app-ttyd/root/usr/share/luci/menu.d/luci-app-ttyd.json
@@ -133,7 +133,7 @@ sed -i '\#export ENV=/etc/shinit#a export HISTCONTROL=ignoredups' package/base-f
 
 # files
 mkdir files
-cp -rf OpenBox/files/* ./files/
+cp -rf ../OpenBox/files/* ./files/
 chmod -R +x files
 
 # NTP
